d690cb86de5939e42e9f8fac8c0c5824b6089d93
Eduardo Henrique <dev_eduardo@hotmail.com>
2018-04-12 23:28:03 -0300

    adicionando contados de palavras e contador de caracteres

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,18 @@ let palavras = $('.frase');
 let tot_palavras = palavras.text().split(" ").length;
 
 $('#total-palavras').text(tot_palavras);
+
+
+$('.campo-digitacao').on("input", ()=>{
+    
+    let campo =  $('.campo-digitacao').val();
+    let campo_sem_espaco = campo.replace(/\s+/g,'');
+    
+
+    let total_caracters = campo_sem_espaco.length;
+    $('#contador-caracteres').text(total_caracters);
+
+    let qtde_palavras = $('.campo-digitacao').val().split(/\S+/).length -1;
+    $('#contador-palavras').text(qtde_palavras);
+
+});
